367b6ff1fb00319d50af9260d1fe483dba821363
zhuang1125 <zhangzhuang@163.com>
2020-08-26 12:34:13 +0800

    Update HomeController.java

--- start-site/src/main/java/io/spring/start/site/web/HomeController.java
+++ start-site/src/main/java/io/spring/start/site/web/HomeController.java
@@ -15,6 +15,18 @@
  */
 package io.spring.start.site.web;
 
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.compress.utils.IOUtils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,4 +44,51 @@ public class HomeController {
 		return "forward:index.html";
 	}
 
+	@Value("classpath:springboot-metadata.json")
+	Resource resource;
+
+	@GetMapping("/springboot-metadata.json")
+	public void downloadTemplate(HttpServletResponse response, HttpServletRequest request) {
+		InputStream inputStream = null;
+		ServletOutputStream servletOutputStream = null;
+		try {
+			String filename = "springboot-metadata.json";
+			//String path = "springboot-metadata.json";
+			// org.springframework.core.io.Resource resource =
+			// resourceLoader.getResource("classpath:"+path);
+
+			response.setContentType("application/json");
+			response.addHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+			response.addHeader("charset", "utf-8");
+			response.addHeader("Pragma", "no-cache");
+			String encodeName = URLEncoder.encode(filename, StandardCharsets.UTF_8.toString());
+			response.setHeader("Content-Disposition",
+					"attachment; filename=\"" + encodeName + "\"; filename*=utf-8''" + encodeName);
+
+			inputStream = this.resource.getInputStream();
+			servletOutputStream = response.getOutputStream();
+			IOUtils.copy(inputStream, servletOutputStream);
+			response.flushBuffer();
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			try {
+				if (servletOutputStream != null) {
+					servletOutputStream.close();
+					servletOutputStream = null;
+				}
+				if (inputStream != null) {
+					inputStream.close();
+					inputStream = null;
+				}
+				// 召唤jvm的垃圾回收器
+				System.gc();
+			}
+			catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}	
 }
